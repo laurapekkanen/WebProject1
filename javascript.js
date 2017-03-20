c5e9132d56f13e97aab589080a74fc41c62cab13
Laura Pekkanen <K8966@LABRANET.JAMK.FI>
2017-03-20 10:24:06 +0200

    map changed and js added

--- javascript.js
+++ javascript.js
@@ -1,10 +1,27 @@
 //document ready
+
 $(function() {
-	$("#marker1").click(function(e) {
+$("#trump").click(function(e) {
         $("#content").load("pages/popup1.html");
     });
 });
 
-$(document).ready(function(){
-    $("main").draggable({stack:"div"});
-})
+$("img.close").click(function(e) {
+		$("#content").empty();
+	});
+
+function initMap() {
+        var myLatLng = {lat: 62.242603, lng: 25.747257};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 13,
+          center: myLatLng,
+          gestureHandling: 'cooperative'
+        });
+
+        var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'Hello World!'
+        });
+      }
