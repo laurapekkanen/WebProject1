8e5810f11ac2c921da0ea091070c7f90c55493f7
Laura Pekkanen <K8966@LABRANET.JAMK.FI>
2017-03-20 10:57:47 +0200

    quiz started and stuff

--- javascript.js
+++ javascript.js
@@ -1,15 +1,15 @@
 //document ready
-
+//pop up
 $(function() {
 $("#trump").click(function(e) {
         $("#content").load("pages/popup1.html");
     });
 });
-
+//close pop up
 $("img.close").click(function(e) {
 		$("#content").empty();
 	});
-
+//map
 function initMap() {
         var myLatLng = {lat: 62.242603, lng: 25.747257};
 
@@ -25,3 +25,74 @@ function initMap() {
           title: 'Hello World!'
         });
       }
+//quiz
+var questions = [];		// all questions
+var question; 			// current question asked
+var correct = 0;		// correct answers count
+var count;				// how many questions
+var SPEED = 1000;		// break between questions
+
+// document ready
+$("document").ready(function() {
+	// load json with jquery Ajax
+	$.ajax({
+		url: 'kysymykset.json',
+		success: function(data) {
+			// called when successful
+			//console.log(data);
+			// store questions to questions array
+			questions = data.questions;
+			count = questions.length;
+			showQuestion();
+			// console.log(questions.length);
+		},
+		error: function(request, status, error) {
+			// called when serror
+			console.log("request.status="+request.status+" status="+status+" error="+error);
+		}
+	});
+});
+
+function showQuestion() {
+	// clear previous question
+	$("#questionDiv").empty();
+	// if all questions are asked -> end!
+	if (questions.length == 0) {
+		//alert("Correct answers : " + correct + "/" + count);
+        $("#questionDiv").append("<p class='question'>Result</p>");
+        $("#questionDiv").append("<p class='answer'>"+correct + "/" + count+"</p>");
+		return;
+	}
+	// random index 0 <-> questions.length
+	var questionIndex = Math.floor(Math.random()*(questions.length));
+	var question = questions[questionIndex];
+	// question
+	$("#questionDiv").append("<p class='question'>"+question.question+"</p>");
+	// answers
+	for (var i=0;i<question.answers.length;i++) {
+		var answer = $("<p>").text((i+1)+". "+question.answers[i].answer);
+		answer.addClass("answer");
+		answer.click(function() {
+			// get index (element as child relative to parent), remember question p is here too!
+			//console.log("pressed index:" + $(this).index());
+			var answerIndex = $(this).index() - 1;
+			// is it right or not
+			if (question.answers[answerIndex].right == 1) {
+				// add correct count
+				correct++;
+				$(this).css('background-color', 'green');
+				$(this).css('color', 'white');
+			} else {
+				$(this).css('background-color', 'red');
+				$(this).css('color', 'white');
+			}
+			// remove asked question from array
+			questions.splice(questionIndex,1);
+			// console.log("length = " + questions.length);
+			// jump next question
+			setTimeout(showQuestion, SPEED);
+		});
+		$("#questionDiv").append(answer);
+	}
+
+}
